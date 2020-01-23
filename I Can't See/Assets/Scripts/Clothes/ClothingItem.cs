e4918cfe9706d24c1541437e075152aeb5b22c49
Lewis Hammond <55703082+LewisHammond-uog@users.noreply.github.com>
2020-01-23 12:50:59 +0000

    Clothing Holdable
    - Made clothing a hodable object
    - Added bypassJob Order to clothing for testing

--- I Can't See/Assets/Scripts/Clothes/ClothingItem.cs
+++ I Can't See/Assets/Scripts/Clothes/ClothingItem.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClothingItem : MonoBehaviour
+public class ClothingItem : Holdable
 {
     //Job Info - For Job Manager to register the associated job actions for the job manager
     [SerializeField]
