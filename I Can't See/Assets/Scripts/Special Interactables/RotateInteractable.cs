096937262290b78ae2a13036f66ab1f55cebb7d7
RhysWareham <55702694+RhysWareham@users.noreply.github.com>
2020-01-23 13:14:00 +0000

    Created script for sliding interactables

---
+++ I Can't See/Assets/Scripts/Special Interactables/RotateInteractable.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateInteractable : Interactable
+{
+    [SerializeField]
+    private AudioSource interactableAudioSource;
+    private Vector3 rotateAmount;
+    [SerializeField]
+    private float rotateLimit;
+    [SerializeField]
+    private Vector3 chosenAxis;
+    private Quaternion startRot;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startRot = this.transform.rotation;
+    }
+
+    public override void DoAction(VRHand hand)
+    {
+        //Check if trigger has been pressed to grab sliding Interactable
+        if (hand.GetActionState(hand.VRInputController.GrabAction))
+        {
+
+            //Get the velocity of the hand
+            rotateAmount = hand.CurrentPose.GetVelocity();
+            //Check if the rotation of the Interactable is less than the rotation limit
+            if (Vector3.Distance(this.transform.position + Vector3.Scale(moveAmount, chosenAxis), startPos) < movementLimit)
+            {
+                //Rotate the Interactable with the player's controller
+                
+            }
+
+            //Check if audio source is not null
+            if (interactableAudioSource != null)
+            {
+                //Play Interactable moving sound
+                interactableAudioSource.Play();
+            }
+
+        }
+        interactableAudioSource.Stop();
+    }
+}
+
+
+//Rhys Wareham
---
+++ I Can't See/Assets/Scripts/Special Interactables/SlideInteractable.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideInteractable : Interactable
+{
+    [SerializeField]
+    private AudioSource interactableAudioSource;
+    private Vector3 moveAmount;
+    [SerializeField]
+    private float movementLimit;
+    [SerializeField]
+    private Vector3 chosenAxis;
+    private Vector3 startPos;
+
+    private void Start()
+    {
+        //Get start position of the Interactable
+        startPos = this.transform.position;
+    }
+
+
+    public override void DoAction(VRHand hand)
+    {
+        //Check if trigger has been pressed to grab sliding Interactable
+        if (hand.GetActionState(hand.VRInputController.GrabAction))
+        {
+
+            //Get the velocity of the hand
+            moveAmount = hand.CurrentPose.GetVelocity();
+            //Check if the distance between the Interactable's startPos and position after moving, is less than the movement limit
+            if (Vector3.Distance(this.transform.position + Vector3.Scale(moveAmount, chosenAxis), startPos) < movementLimit) 
+            {
+                //Move the Interactable with the player's controller
+                this.transform.position += Vector3.Scale(moveAmount, chosenAxis);
+            }
+
+            //Check if audio source is not null
+            if (interactableAudioSource != null)
+            {
+                //Play Interactable moving sound
+                interactableAudioSource.Play();
+            }
+
+        }
+        interactableAudioSource.Stop();
+    }
+}
+
+
+//Rhys Wareham
