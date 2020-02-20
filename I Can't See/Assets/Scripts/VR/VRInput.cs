06c521fb7c7997030b00bd56b8bc7fb54d3b290f
LewisHammond-uog <s1800707@connect.glos.ac.uk>
2020-02-20 15:25:30 +0000

    Fixing Rhys Mistake
    Let's not set random variables to protected

--- I Can't See/Assets/Scripts/VR/VRInput.cs
+++ I Can't See/Assets/Scripts/VR/VRInput.cs
@@ -9,7 +9,12 @@ using Valve.VR;
 /// </summary>
 public class VRInput : MonoBehaviour
 {
-
+    /*
+    READ BEFORE ACCESSING THESE VARIABLES !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    Properties for getting the actions that can be used to check if buttons or button combinations are 
+    pressed. Used public properties to refrence these rather than the private variables as these
+    are ONLY set in the editor
+    */
     [SerializeField]
     private SteamVR_Action_Boolean grabAction;
     public SteamVR_Action_Boolean GrabAction { get { return grabAction; } }
@@ -18,11 +23,8 @@ public class VRInput : MonoBehaviour
     private SteamVR_Action_Boolean moveAction;
     public SteamVR_Action_Boolean MoveAction { get { return moveAction; } }
 
-
-    // generic action to be used when a special interation is available
     [SerializeField]
-    protected SteamVR_Action_Boolean specialDpadAction;
-
+    private SteamVR_Action_Boolean specialDpadAction; 
     public SteamVR_Action_Boolean SpecialDpadAction { get { return specialDpadAction; } }
 
     //Set to not destory on load
