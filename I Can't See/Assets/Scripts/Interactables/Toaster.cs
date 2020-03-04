28ea7708d41ec7a39f1f72e53841351ecd786775
Connor47 <s1802408@connect.glos.ac.uk>
2020-03-04 13:47:57 +0000

    Added Curtain for testing

---
+++ I Can't See/Assets/Scripts/Interactables/Toaster.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toaster : Interactable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public override void DoAction(VRHand hand)
+    {
+        
+    }
+}
