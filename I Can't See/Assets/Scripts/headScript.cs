8ba77fb225dc015db616d1f753048b6c5af42a96
Lewis Hammond <s1800707@connect.glos.ac.uk>
2020-03-04 17:29:34 +0000

    Renamed HeadScript
    - Renamed the headscript to use correct capatalisation

--- I Can't See/Assets/Scripts/headScript.cs
+++ I Can't See/Assets/Scripts/headScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class headScript : MonoBehaviour
+public class HeadScript : MonoBehaviour
 {
     [SerializeField]
     private GameObject toothBrush;
