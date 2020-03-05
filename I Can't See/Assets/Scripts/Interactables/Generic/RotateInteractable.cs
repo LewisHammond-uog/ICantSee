1afd9bc046ccca28d29f4aaad17de42247c867fa
Connor47 <s1802408@connect.glos.ac.uk>
2020-03-05 17:11:15 +0000

    Toilet
    - modified rotate interactable to have rotation limits
    - added toilet flush effect using rotate interatable

--- I Can't See/Assets/Scripts/Interactables/Generic/RotateInteractable.cs
+++ I Can't See/Assets/Scripts/Interactables/Generic/RotateInteractable.cs
@@ -6,9 +6,10 @@ public class RotateInteractable : Interactable
 {
     [SerializeField]
     private AudioSource interactableAudioSource;
-    private Vector3 rotateAmount;
     [SerializeField]
-    private float rotateLimit;
+    private float minRotateLimit;
+    [SerializeField]
+    private float maxRotateLimit;
     [SerializeField]
     private Vector3 chosenAxis;
     private Quaternion startRot;
@@ -29,12 +30,17 @@ public class RotateInteractable : Interactable
             //Get the velocity of the hand
             Quaternion handRot = hand.CurrentPose.poseAction.localRotation;
             
-
             //Check if the rotation of the Interactable is less than the rotation limit
-            if (handRot.z - startRot.z < rotateLimit)
+            //if (handRot.z - startRot.z < rotateLimit)
+            //{
+            //    //Rotate the Interactable with the player's controller
+            //    this.transform.rotation = new Quaternion(this.transform.rotation.x, this.transform.rotation.y, handRot.z, 0);
+            //}
+
+            if(Vector3.Distance(this.transform.rotation.eulerAngles + Vector3.Scale(handRot.eulerAngles, chosenAxis), startRot.eulerAngles) > minRotateLimit ||
+               Vector3.Distance(this.transform.rotation.eulerAngles + Vector3.Scale(handRot.eulerAngles, chosenAxis), startRot.eulerAngles) < maxRotateLimit)
             {
-                //Rotate the Interactable with the player's controller
-                this.transform.rotation = new Quaternion(this.transform.rotation.x, this.transform.rotation.y, handRot.z, 0);
+                this.transform.rotation = Quaternion.FromToRotation(this.transform.rotation.eulerAngles, this.transform.rotation.eulerAngles + handRot.eulerAngles);
             }
 
             //Check if audio source is not null
@@ -49,5 +55,5 @@ public class RotateInteractable : Interactable
     }
 }
 
-
 //Rhys Wareham
+// Connor Done
